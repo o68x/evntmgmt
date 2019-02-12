fef7d9894b8e59eaa07415777380818fc2cb3eef
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-02-12 13:04:19 +0100

    devise inst incl boostrap templates

--- db/migrate/20190211120608_create_users.rb
+++ db/migrate/20190211120608_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :encrypted_password
+      # t.string :email
+      # t.string :encrypted_password
       t.string :first_name
       t.string :last_name
       t.text :description
