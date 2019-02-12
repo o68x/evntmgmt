54f19f65a598920d7c6286ef6b58433ce71af1ab
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-02-12 15:25:56 +0100

    generate controller events

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,25 @@ require 'faker'
 Faker::Config.locale = 'fr'
 
 20.times do |index|
-  s = User.create(email: "testme@yopmail.com", encrypted_password: "HIDDEN", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Hipster.paragraph)
+  s = User.create(email: Faker::Internet.email,
+      password: Faker::Internet.password, 
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      description: Faker::Hipster.paragraph)
 end
 
 10.times do |index|
-  s = Event.create(start_date: Faker::Date.forward(15), duration: rand(12..48) * 5, title: Faker::Quote.robin, description: Faker::Hipster.paragraph, price: rand(50..400), location: Faker::Restaurant.name, user_id: User.all.sample.id)
+  s = Event.create(start_date: Faker::Date.forward(15),
+      duration: rand(12..48) * 5,
+      title: Faker::Quote.robin,
+      description: Faker::Hipster.paragraph,
+      price: rand(50..400),
+      location: Faker::Restaurant.name,
+      user_id: User.all.sample.id)
 end
 
 50.times do |index|
-  s = Attendance.create(stripe_customer_id: "HIDDEN", user_id: User.all.sample.id, event_id: Event.all.sample.id)
+  s = Attendance.create(stripe_customer_id: "HIDDEN",
+      user_id: User.all.sample.id,
+      event_id: Event.all.sample.id)
 end
