5f3d86000bc975bd441a6af4184440a4e916249e
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-02-13 09:44:11 +0100

    some things

--- spec/views/events/index.html.erb_spec.rb
+++ spec/views/events/index.html.erb_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe "events/index", type: :view do
 
   before(:each) do
-    @events = FactoryBot.build_list(:event, 5)
+    # @events = FactoryBot.build_list(:event, 5)
   end
-  
+
   # FIXME (@LUCIEN) Link ok but test fucking fails! And why does it test the whole shit when I just want to check the navbar?
   it "renders the _navbar partial" do
     render
-    expect(response).to render_template(partial: '_navbar')
+    expect(response).to render_template(partial: 'navbar')
   end
-  
+
   describe "#navbar" do
 
     context "when user is autenticated" do
