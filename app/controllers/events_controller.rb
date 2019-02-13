e009918d3f8e829404062bd7808ec0d7c5d442dd
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-02-13 03:39:28 +0100

    chaipu

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -16,6 +16,19 @@ class EventsController < ApplicationController
   end
 
   def create
+    puts '=' * 50 #ça affiche une ligne de 60 symboles $ facilement visible dans le terminal
+    puts 'create event'
+    puts params #tu sais que params doit s'afficher entre les 2
+    puts '=' * 50
+    @event = Event.new(event_params)
+    
+    if @event.save # essaie de sauvegarder en base @gossip
+      flash[:success] = "Event bien enregistré ! Bravo, top !"
+      redirect_to @event
+    else
+      flash[:warning] = "Ooops, comme un blème.."
+      render "new"
+    end
   end
 
   def show
@@ -23,3 +36,10 @@ class EventsController < ApplicationController
   end
 
 end
+
+private
+
+def event_params
+  params.require(:event).permit(:title, :description, :start_date, :duration, :user_id)
+end
+
